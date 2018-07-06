fb88888b978ab28e59a3eb5b38d43d8deaa75080
gronki <gronki@gmail.com>
2018-07-06 19:52:09 +0200

    library installation

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup (
     name = 'pydiskvert',
-    version = '180314',
+    version = '180706',
     author = 'Dominik Gronkiewicz',
     author_email = 'gronki@gmail.com',
     description = u"Calculate vertical structure of accretion disks",
