fcbf24f9c1f2ad5e253430f15381a63a2bb515ab
gronki <gronki@gmail.com>
2020-07-15 22:44:12 +0200

    col2python for sequences

--- python/diskvert/col2python.py
+++ python/diskvert/col2python.py
@@ -3,14 +3,12 @@
 from diskvert.pyminiconf import pyminiconf
 import re
 
-DATFILE_PATTERN = re.compile(r'^(.*)\.(dat|tar\.gz|tgz)(?:\[([0-9]+)\]|)$')
+DATFILE_PATTERN = re.compile(r'^(.*)\.(dat|tar\.gz|tgz)')
+DATSQNC_PATTERN = re.compile(r'^(.*)\.([0-9][0-9][0-9])')
 
 def dat2imagefn(fn, ext = 'png'):
-    fn0, ex0, ix0 = re.match(DATFILE_PATTERN, fn).groups()
-    if ix0:
-        return '{b}.{n:03d}.{f}'.format(b = fn0, n = int(ix0), f = ext)
-    else:
-        return '{b}.{f}'.format(b = fn0, f = ext)
+    fn0, ex0 = re.match(DATFILE_PATTERN, fn).groups()
+    return fn0 + '.' + ext
 
 def read_dtype(f):
     from numpy import dtype
@@ -22,26 +20,30 @@ def readcd(fc,fd):
     return loadtxt(fd, skiprows=2, dtype=dt)
 
 def col2python(fn):
-    from tarfile import open as taropen
-    rexpm = re.search(DATFILE_PATTERN, fn)
-    if rexpm != None:
-        base, ext, nr = rexpm.groups()
-        fde = '.{0:03d}.dat'.format(int(nr)) if nr else '.dat'
-        # print base + fde
-        if ext == 'tar.gz' or ext == 'tgz':
-            with taropen(fn,"r:gz") as tar:
-                fc = tar.extractfile(base + '.col')
-                fd = tar.extractfile(base + fde)
-                ft = tar.extractfile(base + '.txt')
-                return readcd(fc,fd), pyminiconf(ft)
-            # end with
-        elif ext == 'dat':
-            with    open(base + fde,'r') as fd, \
-                    open(base + '.col','r') as fc, \
-                    open(base + '.txt','r') as ft:
-                return readcd(fc,fd), pyminiconf(ft)
-            # end with
-        # end if
+    m = re.match(DATFILE_PATTERN, fn)
+    if m == None: raise Exception("It should be .tar.gz, .tgz or .dat file, got: {}.".format(fn))
+
+    base, ext = m.groups()
+    
+    m = re.match(DATSQNC_PATTERN, base)
+    if m:
+        base, nr = m.groups()
+        nr = int(nr)
+        fde = '.{:03d}.dat'.format(nr)
     else:
-        raise Exception("It should be .tar.gz, .tgz or .dat file, got: %s." % fn)
-    # end if
+        fde = '.dat'
+
+    # print base + fde
+    if ext == 'tar.gz' or ext == 'tgz':
+        from tarfile import open as taropen
+        with taropen(fn,"r:gz") as tar:
+            fc = tar.extractfile(base + '.col')
+            fd = tar.extractfile(base + fde)
+            ft = tar.extractfile(base + '.txt')
+            return readcd(fc,fd), pyminiconf(ft)
+    elif ext == 'dat':
+        with    open(base + fde,'r') as fd, \
+                open(base + '.col','r') as fc, \
+                open(base + '.txt','r') as ft:
+            return readcd(fc,fd), pyminiconf(ft)
+    else: raise Exception("I cannot read {} extension.".format(ext))
